e493ea581934b0338ec60a7d3d3b061099fabfbb
Mark <75133432+Mark-NC001@users.noreply.github.com>
2024-12-06 15:29:56 +0000

    Add files via upload

---
+++ BorderVsFrame/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace BorderVsFrame
+{
+	public partial class MainPage : ContentPage
+	{
+
+		public MainPage()
+		{
+			InitializeComponent();
+
+			TapGestureRecognizer tapGestureRecognizer = new TapGestureRecognizer();
+			tapGestureRecognizer.Tapped += TapGestureRecognizer_Tapped;
+
+			MyFrame.GestureRecognizers.Add(tapGestureRecognizer);
+			MyBorder.GestureRecognizers.Add(tapGestureRecognizer);
+
+		}
+
+		private async void TapGestureRecognizer_Tapped(object? sender, TappedEventArgs e)
+		{
+			await DisplayAlert("Alert", "Control tapped!", "OK");
+		}
+	}
+
+}
